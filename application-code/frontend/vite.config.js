51ce86d09b4456987683a5e58c8eab8eff3696a0
Amol Sontakke <amolsontakke96@gmail.com>
2025-09-27 09:34:13 +0000

    test

--- application-code/frontend/vite.config.js
+++ application-code/frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // Load API URL from env, fallback to node-backend (K8s service name)
-const API_URL = process.env.VITE_API_URL || "http://node-backend:4000";
+const API_URL = process.env.VITE_API_URL || "http://node-backend.backend.svc.cluster.local:4000";
 
 export default defineConfig({
   plugins: [react()],
